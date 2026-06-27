20c435e462d2dc5de97cc0db642ffad014242229
ayoubdaoudixx <ayoubdaoudi2001@gmail.com>
2026-06-27 20:43:17 +0300

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medcareai_chatbot",
+    version="0.1.0",
+    author="Ayoub Daoudi",
+    author_email="ayoubdaoudi2001@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
